75cad4a8fcb2a3e0b9ac6407b525351c9b6e85c3
moiz064 <moizr064@gmail.com>
2023-08-10 19:28:16 +0500

    feat: hero complete

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { background } from '../assets'
 
 const Hero = () => (
-    <div className='flex mx-[3%] h-[720px] rounded-bl-[100px]'
-        style={{backgroundImage: `url(${background})`}}
-    >
-    <div className='mt-[20%] ml-[17%]'>
-    <h1 className='text-6xl text-white'>We see the future of logistics <br /> through development</h1>
-    <button className='h-14 w-80 bg-[#eb6547] mt-4 rounded-tr-[25px] tracking-widest text-xl'>View our portfolio</button>    
+  <div className='relative flex mx-[20px] h-[650px] rounded-bl-[100px]'
+    style={{ backgroundImage: `url(${background})` }}
+  >
+    <div className='absolute bottom-[20%] left-[5%]'>
+      <h1 className='xl:text-6xl text-4xl text-white'>We see the future of logistics <br /> through development</h1>
+      <button className='xl:h-14 h-10 lg:w-80 w-[200px] bg-[#eb6547] mt-4 rounded-tr-[25px] tracking-widest xl:text-xl text-xs font-semibold'>VIEW OUR PORTFOLIO</button>
     </div>
-    </div>
-  )
+  </div>
+)
 
 export default Hero
